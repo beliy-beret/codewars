e04a435646b28a1d7364d2b35a98189e3fb365ce
AlucarD <skoriy12990@gmail.com>
2023-01-27 19:59:34 +0300

    findOddInt

---
+++ findOdd.js
@@ -0,0 +1,15 @@
+/*
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+Examples
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+*/
+
+function findOdd(arr) {
+    return arr.find((item) => arr.filter(el => el == item).length % 2)
+}
