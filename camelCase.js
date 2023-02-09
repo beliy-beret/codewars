22d528e90dc499e23638db257afb99cd2dfb6b5c
AlucarD <skoriy12990@gmail.com>
2023-02-09 19:14:07 +0300

    refactoring camelCase file

--- camelCase.js
+++ camelCase.js
@@ -9,10 +9,3 @@ For instance:
 String.prototype.camelCase = function () {
     return this.split(' ').map(el => el.length ? el[0].toUpperCase() + el.slice(1) : el).join('')    
 }
-
-// function camelCase(str){
-//     let arr = str.split(' ')
-//     let arr2 = arr.map(el => `${el[0].toUpperCase()}${el.substring(1)}`)
-//     return arr2.join('')
-// }
-console.log(" camel case method".camelCase())
